083bf0eff1dfd96a1e06acff81f6d21337cb4b8e
David Jordan <jordand95@gmail.com>
2013-12-03 12:33:49 -0500

    Updated some Code, Removed Batch Files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-Midterm-Project
-===============
+CSI-120
+===================
+Gold Team's Web App
+===================
+David Jordan, Matt Carroll, Jeff Delucia
--- server.js
+++ server.js
@@ -44,46 +44,39 @@ function isValidHashtag(tag) {
 }
 
 // Return a list of ten tweets from the results page.
-function getTweets(req, res, success, error) {
-	if (!isValidHashtag(req.query.hashtag1)
-		|| !isValidHashtag(req.query.hashtag2))
+function getTweets(req, res, success) {
+	var options = {
+		q: "#" + req.query.hashtag1 + " AND #" + req.query.hashtag2,
+		//geocode: 
+		until: req.query.date_to,
+		lang: "en",
+		count: 100,
+		result_type: "recent"
+	};
+	
+	tweets = [];
+	
+	twit.get("/search/tweets.json", options, function(data)
 	{
-		error("Invalid Hashtag(s)");
-	}
-	else {
-		var options = {
-			q: "#" + req.query.hashtag1 + " AND #" + req.query.hashtag2,
-			//geocode: 
-			until: req.query.date_to,
-			lang: "en",
-			count: 100,
-			result_type: "recent"
-		};
-		
-		tweets = [];
-		
-		twit.get("/search/tweets.json", options, function(data)
-		{
-			if (data.statuses && data.statuses.length > 0) {
-				for (var i = 0; i < data.statuses.length && tweets.length < 10; i++) {
-					var raw   = data.statuses[i];
-					
-					tweet = {
-						id: raw.id_str,
-						text: raw.text,
-						score: sentimental(raw.text).score
-					};
-					
-					if (tweet.score >= req.query.score_min
-						&& tweet.score <= req.query.score_max)
-					{
-						tweets.push(tweet);
-					}
+		if (data.statuses && data.statuses.length > 0) {
+			for (var i = 0; i < data.statuses.length && tweets.length < 10; i++) {
+				var raw   = data.statuses[i];
+				
+				tweet = {
+					id: raw.id_str,
+					text: raw.text,
+					score: sentimental(raw.text).score
+				};
+				
+				if (tweet.score >= req.query.score_min
+					&& tweet.score <= req.query.score_max)
+				{
+					tweets.push(tweet);
 				}
 			}
-			success(tweets);
-		});
-	}
+		}
+		success(tweets);
+	});
 }
 
 
@@ -100,9 +93,10 @@ app.get('/', function(req, res) {
 // ==================== SEARCH PAGE ====================== //
 
 app.get("/search", function(req, res) {
-	var content = fs.readFileSync("public/search.html");
-	res.setHeader("Content-Type", "text/html; charset=utf8");
-	res.send(content);
+		var content = fs.readFileSync("public/search.html").toString("utf8");
+		content = content.replace("[MESSAGE]", "");
+		res.setHeader("Content-Type", "text/html");
+		res.send(content);
 });
 
 app.get("/test", function(req, res) {
@@ -132,95 +126,76 @@ app.get('/results', function(req, res) {
 	if (!isValidHashtag(req.query.hashtag1)
 		|| !isValidHashtag(req.query.hashtag2))
 	{
-		/*
-		var content = fs.readFileSync("public/search.html");
-		res.setHeader("Content-Type", "text/html; charset=utf8");
-		res.send(content);
-		*/
-		
 		var content = fs.readFileSync("public/search.html").toString("utf8");
-		
-		//content = content.replace("[TWITTER]", "<h3 style=\"color: red;\">" + message + "</h3>");
-		
+
+		content = content.replace("[MESSAGE]", "<p style=\"color: red;\">Error: Invalid Hashtag(s)</p >");
 		res.setHeader("Content-Type", "text/html");
 		res.send(content);
 	}
 	else {
-	
-	// Success Function:
-	instagram.tags.media(tag1, function (media1, error1) {
-	instagram.tags.media(tag2, function (media2, error2) {
-	getTweets(req, res, function(tweets) {
-		var content = fs.readFileSync("public/results.html").toString("utf8");
-		var list    = "";
-		content     = content.replace("[HASHTAG1]", tag1);
-		content     = content.replace("[HASHTAG2]", tag2);
-		var twitterContent = "";
-		
-		twitterContent += "<h4 style=\"text-align: center;\">Tweets with the hashtags \"";
-		twitterContent += tag1 + "\" and \"" + tag2 + "\"</h4>";
-		list += "<ul data-role=\"listview\" data-divider-theme=\"b\" data-inset=\"true\">";
-		
-		if (tweets.length > 0) {
-			for (var i = 0; i < tweets.length; i++) {
-				tweet = tweets[i];
-				list += "<li data-theme=\"c\"><a href=/tweet/" + tweet.id + ">";
-				list += "[" + sentimental(tweet.text).score + "] ";
-				list += tweet.text;
-				list += "</a></li>";
+		instagram.tags.media(tag1, function (media1, error1) {
+		instagram.tags.media(tag2, function (media2, error2) {
+		getTweets(req, res, function(tweets) {
+			var content = fs.readFileSync("public/results.html").toString("utf8");
+			var list    = "";
+			content     = content.replace("[HASHTAG1]", tag1);
+			content     = content.replace("[HASHTAG2]", tag2);
+			var twitterContent = "";
+			
+			twitterContent += "<h4 style=\"text-align: center;\">Tweets with the hashtags \"";
+			twitterContent += tag1 + "\" and \"" + tag2 + "\"</h4>";
+			list += "<ul data-role=\"listview\" data-divider-theme=\"b\" data-inset=\"true\">";
+			
+			if (tweets.length > 0) {
+				for (var i = 0; i < tweets.length; i++) {
+					tweet = tweets[i];
+					list += "<li data-theme=\"c\"><a href=/tweet/" + tweet.id + ">";
+					list += "[" + sentimental(tweet.text).score + "] ";
+					list += tweet.text;
+					list += "</a></li>";
+				}
 			}
-		}
-		else {
-			twitterContent = "<ul data-role=\"listview\" data-divider-theme=\"b\" data-inset=\"true\">";
-			twitterContent += "<li>No Twitter Results Found</li></ul>"
-		}
-		
-		list += "</ul>";
-		twitterContent += list;
-		
-		content = content.replace("[TWITTER]", twitterContent);
-		
-		// Display the Instagram Image.
-		var instagramContent = "";
-		if (!error1 && media1 && media1.length > 0) {
-			instagramContent += "<h4 style=\"text-align: center;\">Instagram Image with the hashtag \""
-					+ tag1 + "\"</h4>";
-			var url = media1[0].images.thumbnail.url;
-			instagramContent += "<img style=\"text-align: center;\" src="
-					+ url + " style=\"text-align: center;\"></img>";
-		}
-		else {
-			instagramContent += "<p style=\"text-align:center;\">(No instagrams images with the hashtag \""
-					+ tag1 + "\" found)</p>";
-		}
-		if (!error2 && media2 && media2.length > 0) {
-			instagramContent += "<h4 style=\"text-align: center;\">Instagram Image with the hashtag \""
-					+ tag2 + "\"</h4>";
-			var url = media2[0].images.thumbnail.url;
-			instagramContent += "<img style=\"text-align: center;\" src="
-					+ url + " style=\"text-align: center;\"></img>";
-		}
-		else {
-			instagramContent += "<p style=\"text-align:center;\">(No instagrams images with the hashtag \""
-					+ tag2 + "\" found)</p>";
-		}
-		content = content.replace("[INSTAGRAM]", instagramContent);
-		
-		res.setHeader("Content-Type", "text/html");
-		res.send(content);
-	},
-	
-	// Error Function:
-	function(message) {
-		var content = fs.readFileSync("public/search.html").toString("utf8");
-		
-		//content = content.replace("[TWITTER]", "<h3 style=\"color: red;\">" + message + "</h3>");
-		
-		res.setHeader("Content-Type", "text/html");
-		res.send(content);
-	})
-	})
-	});
+			else {
+				twitterContent = "<ul data-role=\"listview\" data-divider-theme=\"b\" data-inset=\"true\">";
+				twitterContent += "<li>No Twitter Results Found</li></ul>"
+			}
+			
+			list += "</ul>";
+			twitterContent += list;
+			
+			content = content.replace("[TWITTER]", twitterContent);
+			
+			// Display the Instagram Image.
+			var instagramContent = "";
+			if (!error1 && media1 && media1.length > 0) {
+				instagramContent += "<h4 style=\"text-align: center;\">Instagram Image with the hashtag \""
+						+ tag1 + "\"</h4>";
+				var url = media1[0].images.thumbnail.url;
+				instagramContent += "<img style=\"text-align: center;\" src="
+						+ url + " style=\"text-align: center;\"></img>";
+			}
+			else {
+				instagramContent += "<p style=\"text-align:center;\">(No instagrams images with the hashtag \""
+						+ tag1 + "\" found)</p>";
+			}
+			if (!error2 && media2 && media2.length > 0) {
+				instagramContent += "<h4 style=\"text-align: center;\">Instagram Image with the hashtag \""
+						+ tag2 + "\"</h4>";
+				var url = media2[0].images.thumbnail.url;
+				instagramContent += "<img style=\"text-align: center;\" src="
+						+ url + " style=\"text-align: center;\"></img>";
+			}
+			else {
+				instagramContent += "<p style=\"text-align:center;\">(No instagrams images with the hashtag \""
+						+ tag2 + "\" found)</p>";
+			}
+			content = content.replace("[INSTAGRAM]", instagramContent);
+			
+			res.setHeader("Content-Type", "text/html");
+			res.send(content);
+		})
+		})
+		});
 	}
 	/*
 	instagram.tags.media(tag1, function (media1, error1) {
